ed072494c2636276b2bcdd72bef542cbd8138ce6
«Mikhail» <kopfgolova@gmail.com>
2022-05-18 14:16:48 +0300

    add Example 012 Methods 3

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -15,7 +15,7 @@ Method2("Текст сообщения");
 void Method21(string msg, int count)
 {
     int i = 0;
-    while(i < count)
+    while (i < count)
     {
         Console.WriteLine(msg);
         i++;
@@ -37,7 +37,7 @@ string Method4(int count, string text)
 {
     int i = 0;
     string result = String.Empty;
-    while(i < count)              //for(int i = 0; i < count; i++)
+    while (i < count)              //for(int i = 0; i < count; i++)
     {                             //    result = result + text;
         result = result + text;
         i++;
@@ -47,9 +47,9 @@ string Method4(int count, string text)
 string res = Method4(10, "asdf");
 Console.WriteLine(res);
 
-for(int i = 2; i <= 10; i++)
+for (int i = 2; i <= 10; i++)
 {
-    for(int j = 0; j <= 10; j++)
+    for (int j = 0; j <= 10; j++)
     {
         Console.WriteLine($"{i} x {j} = {i * j}");
     }
@@ -64,17 +64,17 @@ for(int i = 2; i <= 10; i++)
 string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
             + "ежели бы вас послали вместо нашего милого Винценгероде, "
             + "вы бы взяли приступом согласие прусского короля. "
-            +"Вы так красноречивы. Вы дадите мне чаю?";
+            + "Вы так красноречивы. Вы дадите мне чаю?";
 
 string Replace(string text, char oldValue, char newValue)
 {
     string result = String.Empty;
     int length = text.Length;
-    for(int i = 0; i < length; i++)
-        {
-            if(text[i] == oldValue) result = result + $"{newValue}";
-            else result = result + $"{text[i]}";
-        }
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
     return result;
 }
 string newText = Replace(text, ' ', '|');
@@ -85,3 +85,47 @@ Console.WriteLine(newText);
 Console.WriteLine();
 newText = Replace(newText, 'В', 'в');
 Console.WriteLine(newText);
+
+//Сортировка массива от min к max.
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+            if (array[j] < array[minPosition])
+                minPosition = j;
+        int temp = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+    }
+
+}
+void SelectionSortMax(int[] array)
+{
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+            if (array[j] > array[maxPosition])
+                maxPosition = j;
+        int temp = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temp;
+    }
+
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
+SelectionSortMax(arr);
+PrintArray(arr);
